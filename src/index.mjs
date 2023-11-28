8686804d3682b14e284c5107891324ac1520e3d5
aihki <laine.nestori.julius@gmail.com>
2023-11-28 13:18:01 +0200

    index.mjs korjauksia

--- src/index.mjs
+++ src/index.mjs
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("src"));
+app.use(express.static(path.join(__dirname)));
 
 app.use(
   cors({
