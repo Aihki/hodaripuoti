18eb8f77130c58ef52fb20a78d6f60621341f33d
VeetiSo <v.sorakivi@gmail.com>
2023-12-13 09:42:30 +0200

    https fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rbdsd2mfleg"
+    "revision": "0.g2scjb8vu4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
